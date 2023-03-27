66316898ca054a679e97201d92cc02fc665fe7c5
dollyzn <natasjesus1@gmail.com>
2023-03-27 16:49:28 -0300

    improvements

--- src/layout/index.js
+++ src/layout/index.js
@@ -61,7 +61,6 @@ const Popup = styled(PopperUnstyled)({
 const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
   ({ theme, open }) => ({
     flexGrow: 1,
-    padding: theme.spacing(3),
     transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,33 +1,31 @@
-import { Paper, Grid, Container } from "@mui/material";
+import { Box, Typography } from "@mui/joy";
 
-import Chart from "../../components/Chart";
-import Deposits from "../../components/Deposits";
-import Orders from "../../components/Orders";
-import TypeEffect from "../../components/TypeEffect";
-import Ia from "../../assets/ia.png";
-import { borderRadius } from "@mui/system";
-import { Typography } from "@mui/joy";
 import useIsMobile from "../../hooks/isMobile";
+import TypeEffect from "../../components/TypeEffect";
+
+import Ia from "../../assets/ia.png";
 
 function Home() {
   const isMobile = useIsMobile();
   return (
-    <Container maxWidth="lg" sx={{ mt: 4, mb: 4, boxSizing: "border-box" }}>
-      <Grid container spacing={3}>
-        {/* Chart */}
-        <Grid item xs={12} md={8} lg={6}>
-          <Typography level="h3">
-            A inteligência artificial é a chave para
-            <TypeEffect />
-          </Typography>
-        </Grid>
-        <Grid item xs={12} md={8} lg={4}>
-          {!isMobile && (
-            <img src={Ia} style={{ borderRadius: "50%", position: "static" }} />
-          )}
-        </Grid>
-      </Grid>
-    </Container>
+    <Box sx={{ display: "flex", mt: 5 }}>
+      <Typography level="h3" sx={{ maxWidth: "600px", ml: "15%", mr: "10%" }}>
+        A inteligência artificial é a chave para
+        <TypeEffect />
+      </Typography>
+
+      {!isMobile && (
+        <img
+          src={Ia}
+          style={{
+            borderRadius: "500px",
+            position: "fixed",
+            marginLeft: "1000px",
+          }}
+          draggable="false"
+        />
+      )}
+    </Box>
   );
 }
 
