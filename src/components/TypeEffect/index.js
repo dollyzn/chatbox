ca358c0f3b225fb173975de037daa5e5c2ed1c74
dollyzn <natasjesus1@gmail.com>
2023-03-31 16:21:31 -0300

    update

--- src/components/TypeEffect/index.js
+++ src/components/TypeEffect/index.js
@@ -3,24 +3,19 @@ import { Typography } from "@mui/joy";
 import "../../App.css";
 
 const staticPhrase =
-  "Inteligência Artificial, a capacidade de uma máquina reproduzir a inteligência humana e";
+  "A Inteligência Artificial é a capacidade de uma máquina reproduzir a inteligência humana e";
 
 const phrases = [
-  "tomar decisões complexas com base em dados e algoritmos.",
-  "realizar tarefas de forma autônoma, sem intervenção humana.",
-  "aprender continuamente, melhorando seu desempenho ao longo do tempo.",
-  "analisar grandes quantidades de dados em tempo real.",
-  "prever comportamentos e resultados futuros com precisão.",
-  "criar novos conhecimentos a partir da análise de dados.",
-  "detectar anomalias e padrões ocultos em grandes conjuntos de dados.",
-  "se adaptar a diferentes situações e ambientes.",
-  "se comunicar com seres humanos por meio de linguagem natural.",
-  "reconhecer imagens e objetos em tempo real.",
-  "identificar tendências e padrões em dados de mercado.",
-  "personalizar experiências de usuário com base em dados de comportamento.",
-  "desenvolver sistemas autônomos para tarefas específicas.",
-  "melhorar a eficiência e reduzir os custos em vários setores.",
-  "ser uma ferramenta poderosa para resolver problemas complexos.",
+  "de executar tarefas e tomar decisões que, até então, só poderiam ser realizadas por seres humanos. Graças aos avanços em ciência e tecnologia, a inteligência artificial é capaz de analisar grandes quantidades de dados, reconhecer padrões complexos, aprender com suas próprias experiências e interagir com humanos de forma cada vez mais natural e intuitiva.",
+  "de simular o pensamento, a percepção, a criatividade e a tomada de decisões dos seres humanos, utilizando algoritmos e modelos matemáticos sofisticados. A inteligência artificial é capaz de resolver problemas que antes pareciam impossíveis, como a detecção de fraudes em sistemas financeiros, a análise de imagens médicas para diagnóstico de doenças e a criação de sistemas de reconhecimento de voz e fala cada vez mais precisos.",
+  "de ampliar as capacidades humanas, permitindo que sejam realizadas tarefas de forma mais rápida, precisa e eficiente do que seria possível de forma manual. A inteligência artificial é utilizada em diversas áreas, como a indústria, o comércio, a medicina, a educação e a segurança, e tem o potencial de transformar radicalmente a maneira como vivemos e trabalhamos.",
+  "de criar sistemas que se adaptam ao ambiente e aprendem a partir de dados para tomar decisões, interpretar informações e realizar tarefas que antes eram exclusivas dos seres humanos. A inteligência artificial é capaz de analisar dados de diferentes fontes, identificar padrões e relações complexas, e criar modelos que ajudam a prever eventos futuros, melhorar a eficiência em processos produtivos e oferecer serviços personalizados aos usuários.",
+  "de possibilitar a criação de máquinas que aprendem e evoluem com o tempo, de forma autônoma, sem a necessidade de intervenção humana. A inteligência artificial é capaz de se adaptar a novas situações, detectar problemas e encontrar soluções para desafios complexos, e pode ser aplicada em diversas áreas, como a robótica, a automação industrial, a logística e o transporte, para melhorar a segurança e a eficiência das operações.",
+  "de transformar a maneira como interagimos com a tecnologia, tornando-a cada vez mais inteligente, intuitiva e acessível. A inteligência artificial é capaz de entender a linguagem natural, reconhecer gestos e expressões faciais, e criar interfaces que permitem uma interação mais natural e fluida entre humanos e máquinas.",
+  "de ajudar a resolver alguns dos maiores desafios que a humanidade enfrenta, como a mudança climática, a escassez de recursos naturais e as desigualdades sociais. A inteligência artificial pode ser utilizada para otimizar processos produtivos, reduzir o consumo de energia e de matéria-prima, e criar soluções inovadoras para problemas sociais complexos.",
+  "de gerar novas oportunidades de emprego e de negócios, à medida que novas aplicações e serviços baseados em inteligência artificial são criados. A inteligência artificial é um dos principais motores da chamada quarta revolução industrial, e tem o potencial de gerar novas formas de trabalho, novos modelos de negócios e novas fontes de renda para as pessoas em todo o mundo.",
+  "de ser uma tecnologia que exige uma reflexão ética e social profunda, devido às implicações que tem para a privacidade, a segurança, a liberdade e a dignidade humana. A inteligência artificial deve ser desenvolvida de forma responsável, levando em conta os valores humanos e os interesses da sociedade como um todo, e não apenas os interesses econômicos ou tecnológicos de poucos.",
+  "de ser uma tecnologia em constante evolução, que demanda um esforço contínuo de pesquisa, desenvolvimento e educação. A inteligência artificial é uma das áreas mais dinâmicas e promissoras da ciência e da tecnologia, e oferece um enorme potencial de impacto positivo para a humanidade, desde que seja desenvolvida de forma colaborativa, transparente e responsável.",
 ];
 
 const TypeEffect = () => {
@@ -48,7 +43,7 @@ const TypeEffect = () => {
 
       if (text === currentPhrase && !isDeleting) {
         setIsDeleting(true);
-        setTypingSpeed(50);
+        setTypingSpeed(40);
       } else if (text === "" && isDeleting) {
         setIsDeleting(false);
         setIndex(index + 1);
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Box,
   Typography,
@@ -9,7 +9,10 @@ import {
   IconButton,
   SvgIcon,
   Divider,
+  Badge,
 } from "@mui/joy";
+
+import { motion } from "framer-motion";
 
 import { AuthContext } from "../../context";
 
@@ -18,13 +21,26 @@ import TypeEffect from "../../components/TypeEffect";
 
 import { ReactComponent as IconGPT } from "../../assets/gpt.svg";
 import { ReactComponent as IconDialogFlow } from "../../assets/dialogflow.svg";
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
 import Ia from "../../assets/ia.png";
 import { useContext } from "react";
 import { Link as RouterLink } from "react-router-dom";
 
 function Home() {
   const { user } = useContext(AuthContext);
+  const [showCard, setShowCard] = useState(false);
+  const [hovered, setHovered] = useState(false);
   const isMobile = useIsMobile();
+
+  const iconVariants = {
+    down: { y: -150, transition: { duration: 0.5 } },
+    up: { y: 0, transition: { duration: 0.5 } },
+  };
+
+  const cardVariants = {
+    visible: { opacity: 1, y: 0 },
+    hidden: { opacity: 0, y: 20 },
+  };
 
   const firstName = user?.displayName.split(" ")[0];
 
@@ -69,85 +85,130 @@ function Home() {
               {isMobile ? "Olá!" : `Olá! ${firstName}.`}
             </Typography>
             <div style={{ display: "flex", gap: 15 }}>
-              <RenderLink
-                variant="outlined"
-                to="/chat"
-                icon={<SvgIcon component={IconGPT} inheritViewBox></SvgIcon>}
-                size="lg"
-                fullWidth
-              ></RenderLink>
-              <RenderLink
-                variant="outlined"
-                to="/chat"
-                icon={
-                  <SvgIcon component={IconDialogFlow} inheritViewBox></SvgIcon>
-                }
-                size="lg"
-                fullWidth
-              ></RenderLink>
+              <Badge badgeContent="!">
+                <RenderLink
+                  variant="outlined"
+                  to="/chat"
+                  icon={<SvgIcon component={IconGPT} inheritViewBox></SvgIcon>}
+                  size="lg"
+                  fullWidth
+                ></RenderLink>
+              </Badge>
+              <Badge badgeContent="!">
+                <RenderLink
+                  variant="outlined"
+                  to="/chat"
+                  icon={
+                    <SvgIcon
+                      component={IconDialogFlow}
+                      inheritViewBox
+                    ></SvgIcon>
+                  }
+                  size="lg"
+                  fullWidth
+                ></RenderLink>
+              </Badge>
             </div>
           </Box>
           <Divider sx={{ mt: 2, mb: 3 }} />
           <TypeEffect />
-          <Card
-            variant="outlined"
-            orientation="horizontal"
-            sx={{
+          <motion.div
+            initial={{ y: 0 }}
+            animate={hovered ? "down" : "up"}
+            transition={{
+              type: "spring",
+              stiffness: 50,
+              damping: 10,
+              yoyo: Infinity,
+            }}
+            variants={iconVariants}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            style={{
+              position: "absolute",
+              bottom: 20,
+              left: "49%",
+            }}
+          >
+            <IconButton
+              aria-label="Show card"
+              onClick={() => {
+                setShowCard(!showCard);
+                setHovered(!hovered);
+              }}
+            >
+              <ExpandLessIcon />
+            </IconButton>
+          </motion.div>
+          <motion.div
+            initial="hidden"
+            animate={showCard ? "visible" : "hidden"}
+            variants={cardVariants}
+            transition={{ duration: 0.5 }}
+            style={{
               position: "absolute",
               bottom: 20,
               left: "auto",
               right: "auto",
               width: "calc(100% - 31px)",
-              gap: 2,
-              "&:hover": {
-                boxShadow: "md",
-                borderColor: "neutral.outlinedHoverBorder",
-              },
+              display: showCard ? "block" : "none",
             }}
           >
-            <AspectRatio ratio="1" sx={{ width: 90 }}>
-              <img
-                src="https://avatars.githubusercontent.com/u/66445721?v=4"
-                srcSet="https://avatars.githubusercontent.com/u/66445721?v=4"
-                loading="lazy"
-                alt=""
-              />
-            </AspectRatio>
-            <div>
-              <Typography
-                level="h2"
-                fontSize="lg"
-                id="card-description"
-                mb={0.5}
-              >
-                Natã Santos
-              </Typography>
-              <Typography
-                fontSize="sm"
-                aria-describedby="card-description"
-                mb={1}
-              >
-                <Link
-                  overlay
-                  underline="none"
-                  href="https://github.com/dollyzn"
-                  sx={{ color: "text.tertiary" }}
-                  rel="noopener"
-                  target="_blank"
+            <Card
+              variant="outlined"
+              orientation="horizontal"
+              sx={{
+                gap: 2,
+                "&:hover": {
+                  boxShadow: "md",
+                  borderColor: "neutral.outlinedHoverBorder",
+                },
+              }}
+            >
+              <AspectRatio ratio="1" sx={{ width: 90 }}>
+                <img
+                  src="https://avatars.githubusercontent.com/u/66445721?v=4"
+                  srcSet="https://avatars.githubusercontent.com/u/66445721?v=4"
+                  loading="lazy"
+                  alt=""
+                />
+              </AspectRatio>
+              <div>
+                <Typography
+                  level="h2"
+                  fontSize="lg"
+                  id="card-description"
+                  mb={0.5}
                 >
-                  Técnico Informática - IFF
-                </Link>
-              </Typography>
-              <Chip
-                variant="outlined"
-                color="primary"
-                size="sm"
-                sx={{ pointerEvents: "none" }}
-              >
-                Code, coffee, repeat.
-              </Chip>
-            </div>
-          </Card>
+                  Natã Santos
+                </Typography>
+                <Typography
+                  fontSize="sm"
+                  aria-describedby="card-description"
+                  mb={1}
+                >
+                  <Link
+                    overlay
+                    underline="none"
+                    href="https://github.com/dollyzn"
+                    sx={{ color: "text.tertiary" }}
+                    rel="noopener"
+                    target="_blank"
+                  >
+                    Técnico Informática - IFF
+                  </Link>
+                </Typography>
+                <Chip
+                  variant="outlined"
+                  color="primary"
+                  size="sm"
+                  sx={{ pointerEvents: "none" }}
+                >
+                  Code, coffee, repeat.
+                </Chip>
+              </div>
+            </Card>
+          </motion.div>
         </Card>
       </Box>
       {!isMobile && (
